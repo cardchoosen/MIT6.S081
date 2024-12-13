8f4d002463f815e204f0cb2cea3e0f434e8c16cc
zhangnan.2002 <zhangnan.2002@bytedance.com>
2024-12-13 14:30:32 +0800

    thread lab done

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t *locks;
 
 
 double
@@ -39,11 +40,12 @@ insert(int key, int value, struct entry **p, struct entry *n)
 static 
 void put(int key, int value)
 {
+  
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&locks[i]);
   // is the key already present?
   struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
+  for (e = table[i]; e!= 0; e = e->next) {
     if (e->key == key)
       break;
   }
@@ -54,20 +56,20 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&locks[i]);
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&locks[i]);
   return e;
 }
 
@@ -105,7 +107,6 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
-
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
@@ -116,6 +117,11 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  locks = malloc(sizeof(pthread_mutex_t) * NBUCKET);
+  for(int i = 0; i < NBUCKET; i++) {
+   pthread_mutex_init(&locks[i], NULL);
   }
 
   //
